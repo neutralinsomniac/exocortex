b81f45eb11917a0ffa1375776e9f9ef1c757f50b
Jeremy O'Brien <neutral@fastmail.com>
2020-02-05 20:53:34 -0500

    cascade ref deletions

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS "ref" (
 	"tag_id"	INTEGER NOT NULL,
 	"row_id"	INTEGER NOT NULL,
-	FOREIGN KEY("row_id") REFERENCES "row"("id"),
+	FOREIGN KEY("row_id") REFERENCES "row"("id") ON DELETE CASCADE,
 	PRIMARY KEY("tag_id","row_id"),
-	FOREIGN KEY("tag_id") REFERENCES "tag"("id")
+	FOREIGN KEY("tag_id") REFERENCES "tag"("id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "tag" (
 	"id"	INTEGER,
