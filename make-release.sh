c5f5f51b3707b29711729719fd519c0ad46ee436
Jeremy O'Brien <neutral@fastmail.com>
2020-04-08 09:06:09 -0400

    compile exotui against static sqlite3

--- make-release.sh
+++ make-release.sh
@@ -2,5 +2,5 @@
 mkdir -p build
 CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o build/exogio-windows-amd64.exe -ldflags="-H windowsgui" ./cmd/exogio
 CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o build/exotui-windows-amd64.exe ./cmd/exotui
-GOOS=linux GOARCH=amd64 go build -o build/exotui-linux-amd64 ./cmd/exotui
+GOOS=linux GOARCH=amd64 go build -ldflags '-linkmode external -extldflags -static -w' -o build/exotui-linux-amd64 ./cmd/exotui
 GOOS=linux GOARCH=amd64 go build -o build/exogio-linux-amd64 ./cmd/exogio
