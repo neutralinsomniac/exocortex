1778e22f70f8ee1874e637205eb24a8f5951b7e9
Jeremy O'Brien <neutral@fastmail.com>
2026-03-22 00:13:54 -0400

    use build-go-cache

--- flake.nix
+++ flake.nix
@@ -4,33 +4,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    build-go-cache.url = "github:numtide/build-go-cache";
+    build-go-cache.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      build-go-cache,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        # Import nixpkgs for the specific system
         pkgs = import nixpkgs { inherit system; };
         lib = pkgs.lib;
+        buildGoCache = build-go-cache.legacyPackages.${system}.buildGoCache;
+        vendorHash = "sha256-yQuQ7/jDmKev/WxorsclgJOupPM3hHvgj3RPBU1sPQw=";
+        src = lib.cleanSource ./.;
+
+        goCache = buildGoCache {
+          importPackagesFile = ./imported-packages;
+          inherit src vendorHash;
+        };
       in
       {
         packages.exo = pkgs.buildGoModule {
           name = "exo";
           version = "1.1.0";
 
-          src = lib.cleanSource ./.;
+          inherit src;
 
           subPackages = [ "cmd/exo" ];
 
           doCheck = false;
 
-          vendorHash = "sha256-Kx80kQ5xSFBuaG3ey0n3oX3g5gOvphtuLPNP+qRROCQ=";
+          buildInputs = [ goCache ];
+
+          inherit vendorHash;
         };
 
         devShell = pkgs.mkShell {
